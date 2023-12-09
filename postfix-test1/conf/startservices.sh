8d07a67e935e5f9061dbe4b3e0b6170cb696736c
OMAR RAMON MONTES <omarmontes.879@gmail.com>
2023-12-09 08:33:29 -0500

    docker

--- postfix-test1/conf/startservices.sh
+++ postfix-test1/conf/startservices.sh
@@ -2,7 +2,12 @@
 cp /main.cf /etc/postfix/main.cf
 /bin/echo "Adding the domain ${1} to postfix main.cf config.";
 echo "myhostname = ${1}" >> /etc/postfix/main.cf
+
+echo "disable_plaintext_auth = no" >> /etc/dovecot/conf.d/10-auth.conf
+echo "mail_location = maildir:~/Maildir" >> /etc/dovecot/conf.d/10-mail.conf
+
 service rsyslog start;
 service postfix start;
+service dovecot start;
 sleep 20;
 tail -f /var/log/mail.log
