128d5e24413796c43b69974a1a46e719133a8ffe
OMAR RAMON MONTES <omarmontes.879@gmail.com>
2023-12-09 09:59:44 -0500

    PRING

--- postfix-test1/conf/startservices.sh
+++ postfix-test1/conf/startservices.sh
@@ -5,11 +5,14 @@ cp /main.cf /etc/postfix/main.cf
 
 # hostnamectl set-hostname omarrm.com
 
+/bin/echo "CONFIGURANDO TODO.";
 echo "disable_plaintext_auth = no" >> /etc/dovecot/conf.d/10-auth.conf
 echo "mail_location = maildir:~/Maildir" >> /etc/dovecot/conf.d/10-mail.conf
 
+/bin/echo "REINICIANDO TODO.";
 service rsyslog start;
 service postfix start;
 service dovecot start;
-#sleep 20;
-#tail -f /var/log/mail.log
+/bin/echo "FINALIZÓ TODO.";
+sleep 20;
+tail -f /var/log/mail.log
