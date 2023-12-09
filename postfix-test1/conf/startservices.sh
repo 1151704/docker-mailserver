31b8e7478589d77fbe96357a287fefee89335a6a
OMAR RAMON MONTES <omarmontes.879@gmail.com>
2023-12-09 08:41:11 -0500

    test

--- postfix-test1/conf/startservices.sh
+++ postfix-test1/conf/startservices.sh
@@ -3,6 +3,8 @@ cp /main.cf /etc/postfix/main.cf
 # /bin/echo "Adding the domain ${1} to postfix main.cf config.";
 # echo "myhostname = ${1}" >> /etc/postfix/main.cf
 
+hostnamectl set-hostname omarrm.com
+
 echo "disable_plaintext_auth = no" >> /etc/dovecot/conf.d/10-auth.conf
 echo "mail_location = maildir:~/Maildir" >> /etc/dovecot/conf.d/10-mail.conf
 
